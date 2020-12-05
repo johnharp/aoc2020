078b29de23b892601f3d3692ade08345da39ee1b
John Harper <john@harper.link>
2020-12-05 10:33:23 -0600

    day 5

--- day05a/src/link/harper/Main.java
+++ day05a/src/link/harper/Main.java
@@ -7,15 +7,47 @@ public class Main {
         input.open("input.txt");
 
         Calculator calc = new Calculator();
-        int maxSeatId = 0;
 
-        for(String line = input.line(); line != null; line = input.line()) {
+//        Part A --------------------------------------------------------------
+//        int maxSeatId = 0;
+//
+//        for(String line = input.line(); line != null; line = input.line()) {
+//            int seatId = calc.SeatId(line);
+//            if (seatId > maxSeatId) {
+//                maxSeatId = seatId;
+//            }
+//        }
+//
+//        System.out.println("Max seat ID = " + maxSeatId);
+
+
+//        Part B ---------------------------------------------------------------
+        int minOccupiedSeatId = Integer.MAX_VALUE;
+        int maxOccupiedSeatId = Integer.MIN_VALUE;
+        boolean[] occupiedSeats = new boolean[1024];
+
+        for (String line = input.line(); line != null; line = input.line()) {
             int seatId = calc.SeatId(line);
-            if (seatId > maxSeatId) {
-                maxSeatId = seatId;
+
+            if (seatId > maxOccupiedSeatId) {
+                maxOccupiedSeatId = seatId;
+            }
+
+            if (seatId < minOccupiedSeatId) {
+                minOccupiedSeatId = seatId;
+            }
+
+            occupiedSeats[seatId] = true;
+        }
+
+        System.out.println("Min seat ID = " + minOccupiedSeatId);
+        System.out.println("Max seat ID = " + maxOccupiedSeatId);
+
+        for (int id = minOccupiedSeatId+1; id < maxOccupiedSeatId; id++) {
+            if (occupiedSeats[id] == false) {
+                System.out.println("Seat ID " + id + " is free");
             }
         }
 
-        System.out.println("Max seat ID = " + maxSeatId);
     }
 }
