b1c100f804f05957db36bf16be7e77e98ea65825
John Harper <john@harper.link>
2020-12-14 16:13:45 -0600

    day14

--- day14/src/link/harper/Computer.java
+++ day14/src/link/harper/Computer.java
@@ -1,8 +1,14 @@
 package link.harper;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.List;
 
 public class Computer {
+    private final int numBits = 64;
+
     public Hashtable<Long, Long> getMemory() {
         return memory;
     }
@@ -54,5 +60,63 @@ public class Computer {
         }
 
         return sum;
+    }
+
+    public void handlePart2(String mask, long address, long value) throws Exception{
+        String addressString = Long.toBinaryString(address);
+        addressString =  padLeftChar(addressString, 36, '0');
+
+        char[] maskChars = mask.toCharArray();
+        char[] addressChars = addressString.toCharArray();
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i<maskChars.length; i++) {
+            if (maskChars[i] == '0') sb.append(addressChars[i]);
+            else if (maskChars[i] == '1') sb.append('1');
+            else if (maskChars[i] == 'X') sb.append('X');
+            else throw new Exception("Unknown mask character: '" + maskChars[i] + "'");
+        }
+
+        List<String> addressStrings = expandAddress(sb.toString());
+
+        for(String addressStr: addressStrings) {
+            long expandedAddress = addressFromString(addressStr);
+            write(expandedAddress, value);
+        }
+    }
+
+    public List<String> expandAddress(String address) {
+        List<String> expanded = new ArrayList<>();
+
+        if (address.contains("X")) {
+            String[] parts = address.split("X", 2);
+            String address1 = parts[0] + "0" + parts[1];
+            String address2 = parts[0] + "1" + parts[1];
+            expanded.addAll(expandAddress(address1));
+            expanded.addAll(expandAddress(address2));
+        } else {
+            expanded.add(address);
+        }
+
+        return expanded;
+    }
+
+    public long addressFromString(String str) {
+        return Long.parseLong(str, 2);
+    }
+
+
+
+    public String padLeftChar(@NotNull String str, int length, char pad) {
+        if (str.length() >= length) {
+            return str;
+        }
+        StringBuilder sb = new StringBuilder();
+        while (sb.length() < length - str.length()) {
+            sb.append(pad);
+        }
+        sb.append(str);
+
+        return sb.toString();
     }
 }
--- day14/src/link/harper/Part2.java
+++ day14/src/link/harper/Part2.java
@@ -1,4 +1,45 @@
 package link.harper;
 
+import java.io.File;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Part2 {
+    private static Pattern maskPattern = Pattern.compile("^mask = ([01X]+)$");
+    private static Pattern memPattern = Pattern.compile("^mem\\[([0-9]+)\\] = ([0-9]+)$");
+
+    private static long thirtySixOnesMask = 68719476735L;
+
+    public static void main(String[] args) throws Exception{
+        //Input in = new Input("example2-input.txt");
+        Input in = new Input("input.txt");
+
+        Computer comp = new Computer();
+        String mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+        //Mask mask = new Mask("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+
+        for(String line : in.getLines()) {
+            Matcher maskMatcher = maskPattern.matcher(line);
+            Matcher memMatcher = memPattern.matcher(line);
+
+            if (maskMatcher.find()) {
+                mask =maskMatcher.group(1);
+            } else if (memMatcher.find()) {
+                long addr = Long.parseLong(memMatcher.group(1));
+                long value = Long.parseLong(memMatcher.group(2));
+
+                comp.handlePart2(mask, addr, value);
+            } else {
+                throw new Exception("Unknown input found: " + line);
+            }
+        }
+
+        comp.coredump();
+
+        long sum = comp.sumAllMemory();
+        System.out.println("Sum of memory = " + sum);
+
+    }
+
 }
